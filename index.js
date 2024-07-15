87b6a226214224c487a3b276af865230de30efbf
Sriram <sriramsubramanian23@gmail.com>
2024-07-15 18:05:34 +0530

    Fixed cors for UI URL

--- index.js
+++ index.js
@@ -7,17 +7,10 @@ const TodoRouter = require('./routes/TodoRoutes')
 const AuthRouter = require('./routes/AuthRoutes')
 
 const app = express()
-const corsOptions = {
-    origin: function (origin, callback) {
-      if (origin === process.env.UI_URL) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    }
-  };
   
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: [process.env.UI_URL]
+}));
 app.use(express.json())
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {
